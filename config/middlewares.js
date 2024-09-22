48b6d71d9c172800dd946236c7e946af9c183042
Bryan Quezada <71483036+BryanQuezada1910@users.noreply.github.com>
2024-09-22 01:19:42 -0600

    Fix middleware for cloudinary

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,14 +1,6 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
   {
     name: 'strapi::security',
     config: {
@@ -23,4 +15,11 @@ module.exports = [
       },
     },
   },
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
 ];
